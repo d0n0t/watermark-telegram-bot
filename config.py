361bf815d59a3568f8951c144e1041c824b9bb14
rking32 <sljohnwick@gmail.com>
2019-12-18 20:51:08 +0530

    __fix__

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-WATERMARK = os.environ.get("WATERMARK", "...watermark...")
-FONT_SIZE = os.environ.get("FONT_SIZE", 15)
+WATERMARK = str(os.environ.get("WATERMARK", "...watermark..."))
+FONT_SIZE = int(os.environ.get("FONT_SIZE", 15))
 FONT_NAME = "Vera_Crouz.ttf"  # check tools/fonts
-TRANSPARENCY = os.environ.get("TRANSPARENCY", 0.75)
-QUALITY = os.environ.get("QUALITY", 100)
+TRANSPARENCY = float(os.environ.get("TRANSPARENCY", 0.75))
+QUALITY = int(os.environ.get("QUALITY", 100))
