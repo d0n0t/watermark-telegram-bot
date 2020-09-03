e9230e1bb11d43ffc0b676c3340b9024c04b2b32
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-09-03 20:36:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ WATERMARK = str(os.environ.get("WATERMARK", "...watermark..."))
 FONT_SIZE = int(os.environ.get("FONT_SIZE", 15))
 FONT_NAME = "Vera_Crouz.ttf"  # check tools/fonts
 TRANSPARENCY = float(os.environ.get("TRANSPARENCY", 0.75))
-QUALITY = int(os.environ.get("QUALITY", 100))
+QUALITY = int(os.environ.get("QUALITY", 1080))
